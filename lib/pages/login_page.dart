a5b6125b67d1bdc274063aceb6f333b9a314c985
adnan ahmed <adnanahmed504423@gmail.com>
2023-07-16 22:37:18 +0530

    day5 completed

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,19 @@
+import 'package:classico/main.dart';
+import 'package:classico/utils/routes.dart';
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Material(
-        color: Colors.white,
+      color: Colors.white,
+      child: SingleChildScrollView(
         child: Column(
           children: [
             Image.asset(
@@ -16,41 +25,40 @@ class LoginPage extends StatelessWidget {
             ),
             Text(
               "Welcome",
-              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             SizedBox(
               height: 20.0,
             ),
             Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+                  const EdgeInsets.symmetric(vertical: 20.0, horizontal: 32.0),
               child: Column(
                 children: [
                   TextFormField(
                     decoration: InputDecoration(
-                        hintText: "Enter username", labelText: "Username"),
+                        hintText: "Enter Username", labelText: "Username"),
                   ),
                   TextFormField(
                     obscureText: true,
                     decoration: InputDecoration(
-                      hintText: "Enter password",
-                      labelText: "Password",
-                    ),
+                        hintText: "Enter Password", labelText: "Password"),
                   ),
                   SizedBox(
-                    height: 20.0,
+                    height: 40.0,
                   ),
                   ElevatedButton(
-                    onPressed: () {
-                      print("Hi codepur");
-                    },
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                  )
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(120, 40))),
                 ],
               ),
             )
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
